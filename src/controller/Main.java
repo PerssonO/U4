28ce96af4279e372bf1631f6c7f3dfab195883f1
jontem1 <jontem94@hotmail.com>
2023-12-13 11:43:26 +0100

    Update Main.java

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -2,25 +2,8 @@ package controller;
 import view.*;
 //import model.*;
 
-// TESTA GIT
-// TEST2
-
-//test3 inte av ola
-
-//Test by Ola
-
-//OLA
-
-//jävla massa test
-
-//TEST 5
 public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller();
     }
-    /**
-     * DETTA ÄR MIN ALLDELES EGNA BRANCH OCH DEN ÄR JÄTTEFIN
-     */
 }
-
-//Test OLAOLAOLA

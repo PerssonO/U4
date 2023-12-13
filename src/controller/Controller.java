8a848921ab956cdf3a2672ef739c387a42e42108
PerssonO <1984.ola.persson@gmail.com>
2023-12-13 20:45:14 +0100

    ola

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -18,6 +18,9 @@ public class Controller {
         this.mainframe = new MainFrame(1000, 550, this);
         this.spelplan = new Spelplan(); //test för att kolla att spelplanen har rätt storlek.
        // mainframe.getMainPanel().getLeftPanel().getButton(0,0).setEnabled(false);
+        //mainframe.getMainPanel().getLeftPanel().getButton(0,0).setBackground(Color.cyan);
+       //mainframe.getMainPanel().getLeftPanel().getButton(0,0).setForeground(Color.cyan);
+
     }
 
     public void buttonPressed(ButtonType button) {
